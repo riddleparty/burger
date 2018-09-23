a5629404dc3cc32f4df17de6efc184442ae1738d
Justin Riddle <justinriddle@icloud.com>
2018-09-23 00:31:31 -0700

    updated files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Boca Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beyond Burger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name) VALUES ('Gardein Burger');
